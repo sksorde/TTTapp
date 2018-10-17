c3a23188d2ee69ce385dfe73da6571194a52a89d
paragshah1979 <44120942+paragshah1979@users.noreply.github.com>
2018-10-17 22:15:57 +0100

    updated js file

--- JS/mongoInsert.js
+++ JS/mongoInsert.js
@@ -20,3 +20,13 @@ var Schema = new mongoose.Schema({
 });
 var user = mongoose.model('emp',Schema);
 
+app.post('/new',function(req,res) {
+	new user({
+		_id  :req.body.email,
+		name: req.body.name,
+		age: req.body.age
+	}).save(function (err,doc) {
+		if(err) res.json(err);
+		else res.send('Success');
+	});
+});
